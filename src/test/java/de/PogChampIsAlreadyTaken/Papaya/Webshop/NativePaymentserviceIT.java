fd23a9f2a471817d15f05a6e64540ec75aaea3a5
studlintmo6711 <moritz.lintterer@stud.th-rosenheim.de>
2021-10-29 16:01:32 +0200

    intial project commit

---
+++ src/test/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/NativePaymentserviceIT.java
@@ -0,0 +1,9 @@
+package de.PogChampIsAlreadyTaken.Papaya.Webshop;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativePaymentserviceIT extends PaymentserviceTest {
+
+    // Execute the same tests but in native mode.
+}
